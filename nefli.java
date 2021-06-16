28d8a3a05f2136562c2ec2fb1a6d3b370d740d7d
Juan Manuel Castagno <49038828+jcastagno99@users.noreply.github.com>
2021-06-16 17:27:45 -0300

    resolución en pseudo-código

---
+++ nefli.java
@@ -0,0 +1,50 @@
+
+
+/*
+Interpretación:
+
+Entiendo que Reproductor tiene 3 listas de listeners (como un observer con multi listas)
+
+A mi interpretación, los listener mediante el idVideo pueden modificar el minutoActual o ultimo minuto donde
+la película estaba siendo visualizada, dicho minuto es tambien un atributo de la película (clase que añado), 
+el StopListener podría buscar la película mediante su ID y decirle: "Tu ultimo minuto antes de pausarte fue este" 
+y setearlo en la película para que luego la reproducción (con PlayListener) use dicho minuto, en la inicialización 
+el minutoActual = 0;
+
+*/
+
+Class Usuario {
+
+    Reproductor miReproductor;
+
+    void continuarViendoPelicula(Pelicula unaPelicula){
+        miReproductor.play(unaPelicula.idVideo,unaPelicula.minutoActual);
+    }
+}
+
+Class Pelicula {
+    ...
+    int idVideo;
+    int minutoActual = 0;
+}
+
+/*
+    Otra opción: Me parece que el método continuarViendoPelicula en usuario es mas un pasamano y que la película
+    cuenta con todo lo necesario para que se encarge de avisarle al reproductor que se la continue reproduciendo
+    aunque particularmente me gustaría añadir el método en Reproductor y pasarle como parametro la película en sí
+    no voy a modificar la interfaz porque entiendo que el enunciado solo quiere que la use.
+
+*/
+
+Class Pelicula{
+
+    int idVideo;
+    int minutoActual = 0;
+    Reproductor miReproductor;
+
+    void continuarViendoPelicula(Pelicula unaPelicula){
+        miReproductor.play(idVideo,minutiActual);
+    }
+
+}
+
